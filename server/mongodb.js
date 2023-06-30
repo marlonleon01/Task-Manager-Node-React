9c52d1dab1168eed2a9c6989a566b9830ec6329c
Marlon Leon <marlon.leon2025@gmail.com>
2023-06-30 16:04:16 -0400

    worked on all the CRUD operations with mongodb now its setup for other uses

--- server/mongodb.js
+++ server/mongodb.js
@@ -7,61 +7,11 @@ const databaseName = "task-manager"
 const connectToDatabase = async () => {
     try {
         await client.connect()
-        console.log("Connected correctly")
-        const db = client.db(databaseName)
-
-        // db.collection("users").insertOne({
-        //     name: "Marlon",
-        //     age: 23
-        // }).then((result, error) => {
-        //     if (error) {
-        //         return console.log("Unabled to insert documents")
-        //     }
-
-        //     console.log(result.acknowledged, result.insertedId)
-        // })
-
-        // db.collection("users").insertMany([
-        //     {
-        //         name: "Jen",
-        //         age: 28
-        //     }, 
-        //     {
-        //         name: "Gunther",
-        //         age: 27
-        //     }
-        // ]).then((result, error) => {
-        //     if (error) {
-        //         return console.log("Unabled to insert documents")
-        //     }
-
-        //     console.log(result.insertedCount)
-        // })
-
-        db.collection("tasks").insertMany([
-            {
-                description: "Clean the house",
-                completed: true
-            },
-            {
-                description: "Go to gym",
-                completed: false
-            },
-            {
-                description: "Walk dogs",
-                completed: true
-            }
-        ]).then((result, error) => {
-            if (error) {
-                return console.log("Unable to insert documents")
-            }
-
-            console.log(result.acknowledged, result.insertedCount, result.insertedIds)
-        })
-
     } catch(error) {
-        console.log("Unabled to connect to the database")
+        console.log(error)
     }
+
+    const db = client.db(databaseName)
 }
 
 connectToDatabase()
