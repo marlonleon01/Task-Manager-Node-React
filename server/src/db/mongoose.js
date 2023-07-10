98c6ede047cdf176cb673ae47d59a69dcf744d83
Marlon Leon <marlon.leon2025@gmail.com>
2023-07-10 15:11:53 -0400

    practice create mongoose model and saving the document

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -4,23 +4,41 @@ const connectionURL = "mongodb://127.0.0.1:27017/task-manager-api"
 
 mongoose.connect(connectionURL)
     
-const User = mongoose.model("User", {
-    name: {
-        type: String,
-    },
-    age: {
-        type: Number,
-    }
-})
+// const User = mongoose.model("User", {
+//     name: {
+//         type: String,
+//     },
+//     age: {
+//         type: Number,
+//     }
+// })
 
-const me = new User({
-    name: "Marlon",
-    age: 23
-})
+// const me = new User({
+//     name: "Marlon",
+//     age: 23
+// })
 
-me.save()
-    .then(() => {
-        console.log(me)
-    }).catch((error) => {
-        console.log("Error", error)
-    })
+// me.save()
+//     .then(() => {
+//         console.log(me)
+//     }).catch((error) => {
+//         console.log("Error", error)
+//     })
+
+// const Tasks = mongoose.model("tasks", {
+//     description: {
+//         type: String,
+//     },
+//     completed: {
+//         type: Boolean,
+//     }
+// })
+
+// const task = new Tasks({
+//     description: "Walk dogs",
+//     completed: true
+// })
+
+// task.save()
+//     .then(() => console.log(task))
+//     .catch(error => console.log(error))
