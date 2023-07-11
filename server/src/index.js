7d0e4f25f2f6ae262f1ad25868381507a92981bd
Marlon Leon <marlon.leon2025@gmail.com>
2023-07-11 17:05:10 -0400

    removed user routes from index.js and added to user.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,86 +1,13 @@
 import express from "express"
 import "./db/mongoose.js"
-import User from "./models/user.js"
 import Task from "./models/task.js"
+import router from "./routers/user.js"
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-
-
-//User Endpoints
-app.post("/users", async (req, res) => {
-    const user = new User(req.body)
-
-    try {
-        await user.save()
-        res.status(201).send(user)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
-
-app.get("/users", async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users) 
-    } catch (error) {
-        res.status(500).send()
-    }
-})
-
-app.get("/users/:id", async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById(_id)
-        
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    } catch (error) {
-        res.status(500).send()
-    }
-})
-
-app.patch("/users/:id", async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ["name", "email", "password", "age"]
-    const isValidOperation = updates.every(update => allowedUpdates.includes(update))
-
-    if (!isValidOperation) {
-        return res.status(400).send({error: "Invalid update!"})
-    }
-
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
-
-app.delete("/users/:id", async (req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id)
-
-        if (!user) {
-            res.status(404).send()
-        }
-
-        res.send(user)
-    } catch (error) {
-        res.status(500).send()
-    }
-})
+app.use(router)
 
 //Tasks Endpoints
 app.post("/tasks", async (req, res) => {
