c12e8a3e45bb57ebf4f63c8e0a731ca0d61f7df6
Marlon Leon <marlon.leon2025@gmail.com>
2023-07-11 17:07:46 -0400

    changed methods to new router name

--- server/src/routers/user.js
+++ server/src/routers/user.js
@@ -3,7 +3,7 @@ import User from "../models/user.js"
 
 const userRouter = new express.Router()
 
-router.post("/users", async (req, res) => {
+userRouter.post("/users", async (req, res) => {
     const user = new User(req.body)
 
     try {
@@ -14,7 +14,7 @@ router.post("/users", async (req, res) => {
     }
 })
 
-router.get("/users", async (req, res) => {
+userRouter.get("/users", async (req, res) => {
     try {
         const users = await User.find({})
         res.send(users) 
@@ -23,7 +23,7 @@ router.get("/users", async (req, res) => {
     }
 })
 
-router.get("/users/:id", async (req, res) => {
+userRouter.get("/users/:id", async (req, res) => {
     const _id = req.params.id
 
     try {
@@ -39,7 +39,7 @@ router.get("/users/:id", async (req, res) => {
     }
 })
 
-router.patch("/users/:id", async (req, res) => {
+userRouter.patch("/users/:id", async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ["name", "email", "password", "age"]
     const isValidOperation = updates.every(update => allowedUpdates.includes(update))
@@ -61,7 +61,7 @@ router.patch("/users/:id", async (req, res) => {
     }
 })
 
-router.delete("/users/:id", async (req, res) => {
+userRouter.delete("/users/:id", async (req, res) => {
     try {
         const user = await User.findByIdAndDelete(req.params.id)
 
