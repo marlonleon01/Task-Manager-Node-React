c57941273f1e549345fc0c4b69776c6df5d04ea9
Marlon Leon <marlon.leon2025@gmail.com>
2023-07-17 18:22:06 -0400

    added userSchema.virtual

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -52,6 +52,12 @@ const userSchema = new mongoose.Schema({
     timestamps: true
 })
 
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
+})
+
 userSchema.methods.generateAuthToken = async function() {
     const user = this
     const token = jwt.sign({_id: user._id.toString()}, "thisismynewcourse")
